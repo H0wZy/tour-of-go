f561ea8307e5c1cf1539c57460e21910cd96e71c
H0wZy <167232856+H0wZy@users.noreply.github.com>
2026-02-18 19:19:32 -0300

    feat: append func ok

--- main.go
+++ main.go
@@ -5,13 +5,23 @@ import (
 )
 
 func main() {
-	Append()
+	IntAppend()
+	StringAppend()
 }
 
 // FUNÇÃO APPEND DEMONSTRANDO COMO USAR A FUNÇÃO APPEND PARA ADICIONAR ELEMENTOS A UM SLICE
-func Append() {
+func IntAppend() {
 	slice := []int{1, 2, 3}
 	slize := []int{4, 5, 6}
+
+	slice = append(slice, slize...)
+
+	fmt.Println("Slice after appending:", slice)
+}
+
+func StringAppend() {
+	slice := []string{"Hello", "World"}
+	slize := []string{"Go", "Programming", "Language"}
 
 	slice = append(slice, slize...)
 
